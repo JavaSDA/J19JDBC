55da0007d4935072fa3f4a20f2b507dff28b3484
WayneJr <40968414+WayneJr@users.noreply.github.com>
2023-01-09 19:56:13 +0100

    hibernate commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,38 @@
-import controllers.Customer;
 import controllers.Item;
 import controllers.Orders;
 import controllers.Sales;
 import controllers.menu.Menu;
+import db.Database;
+import entities.Customer;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 public class Main {
     public static void main(String[] args) {
-        Customer.createCustomerTable();
-        Item.createItemsTable();
-        Sales.createSalesTable();
-        Orders.createOrderTable();
+        controllers.Customer.createCustomerTable();
+//        Customer.createCustomerTable();
+//        Item.createItemsTable();
+//        Sales.createSalesTable();
+//        Orders.createOrderTable();
         // subsequent objects will have their create table
         // methods here
 
-        Menu.mainMenu();
+//        Menu.mainMenu();
+
+        Session session = Database.getHibSesh();
+
+        Customer cust = session.find(Customer.class, 4);
+        System.out.println(cust);
+
+//        Customer barry = new Customer("Barry", "Allen", "barry@gmail.com");
+//
+//        try {
+//            Transaction trans = session.beginTransaction();
+//            session.save(barry);
+//            trans.commit();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
     }
 
     // 25 mins
@@ -28,7 +47,25 @@ public class Main {
     // handleItemsTotal method and log the total to the console.
 
 
+    // 45 mins
+    // Create a table called Students and another table called Grades
+    // The columns on the students table should be as follows: id, name, age
+    // Complete the controllers for the students table by adding methods
+    // handle CRD operations on each record in the table.
 
+    // On the Grades table, store the students id, score and the Grade where
+    // The columns on Grades should be: id, student_id, score, grade and
+    // where for each Grade:
+    // 0 - 40 = F, 41 - 49 = D, 50 - 59 = C, 60 - 69 = B, 70 - 100 = A.
+    // Complete the controller on the Grades table to have Create, Read, Delete Operations
+    // For the read operation, you should be able to see the students information as well
+
+
+
+    // Using Hibernate create the entity object for the items class and
+    // make find queries on the items table to retrieve any items you
+    // previously stored in it.
+    // Also, demonstrate how you would save a new entry into the items table.
 
 
 
